f07e86c8b8159b8b9d11f100589cc2877f566517
Developer Deck 101 <devdeck101@Brunos-MacBook-Pro-2.local>
2017-05-31 04:36:02 -0300

    Inclusão dos arquivos modificados do video Angular 4 - Aplicação Completa - Parte 2

---
+++ src/app/feed/feed.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feed',
+  templateUrl: './feed.component.html',
+  styleUrls: ['./feed.component.css']
+})
+export class FeedComponent implements OnInit {
+
+  tweets = [
+    {
+      user: "stevie",
+      name: "Stevie Gray",
+      body: "Stevie Feliciano is a library scientist living in New York City. She likes to spend her time reading, running, and writing.",
+      avatar: "../assets/images/avatar/stevie.jpg"
+    },
+    {
+      user: "jenny",
+      name: "Jenny Doe",
+      body: "Jenny Doe is a library scientist living in New York City. She likes to spend her time reading, running, and writing.",
+      avatar: "../assets/images/avatar/jenny.jpg"
+    },
+    {
+      user: "veronika",
+      name: "Veronika Sanders",
+      body: "Veronika is a library scientist living in New York City. She likes to spend her time reading, running, and writing.",
+      avatar: "../assets/images/avatar/veronika.jpg"
+    }
+
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
