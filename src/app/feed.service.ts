59aa1ea7ecd63b700ceb0239cbb16b6134147d86
Developer Deck 101 <devdeck101@Brunos-MacBook-Pro-2.local>
2017-06-21 23:13:00 -0300

    Inclusão dos arquivos modificados do video Angular 4 - Aplicação Completa - Parte 6

---
+++ src/app/feed.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Http , Response} from '@angular/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class FeedService {
+
+  url = 'http://localhost:3000/tweets'
+
+  constructor(private http: Http) { }
+
+  getTweets(): Observable<Response>{
+    return this.http.get(this.url);
+  }
+
+  likeTweet(tweet){
+    return this.http.put(`${this.url}/${tweet.id}`, tweet);
+  }
+
+}
--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,72 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FeedService } from '../feed.service';
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
-  styleUrls: ['./feed.component.css']
+  styleUrls: ['./feed.component.css'],
+  providers: [FeedService]
 })
 export class FeedComponent implements OnInit {
 
-  tweets = [
-    {
-      user: "stevie",
-      name: "Stevie Gray",
-      body: "Stevie Feliciano is a library scientist living in New York City. She likes to spend her time reading, running, and writing.",
-      likes: [],
-      avatar: "../assets/images/avatar/stevie.jpg"
-    },
-    {
-      user: "jenny",
-      name: "Jenny Doe",
-      body: "Jenny Doe is a library scientist living in New York City. She likes to spend her time reading, running, and writing.",
-      likes: [],
-      avatar: "../assets/images/avatar/jenny.jpg"
-    },
-    {
-      user: "veronika",
-      name: "Veronika Sanders",
-      body: "Veronika is a library scientist living in New York City. She likes to spend her time reading, running, and writing.",
-      likes: ['jenny', 'stevie'],
-      avatar: "../assets/images/avatar/veronika.jpg"
-    }
-
-  ];
+  tweets;
 
   likedTweet = undefined;
   currentUser = 'stevie';
 
-  constructor() { }
+  constructor(private feedService: FeedService) { }
 
   ngOnInit() {
+    this.feedService.getTweets().subscribe(data => {
+      this.tweets = data.json();
+    })
+    
   }
 
   handleLikedTweetFromChildComponent(tweet){
-    this.likedTweet = tweet;   
-    if( this.isRetweetedByUser(tweet) ){
-      this.findTweet(tweet).likes.push(this.currentUser);
-    }
-  }
-
-  /**
-   * Verify if the user already liked the tweet
-   * @param tweet 
-   */
-  isRetweetedByUser(tweet){
-    return this.findTweet(tweet).likes.findIndex(currUser => {
-      return currUser == currUser
-    }) == -1
-
-  }
-
-  /**
-   * Find the tweeter user
-   * @param tweet
-   */
-  findTweet(tweet){
-    var index = this.tweets.findIndex(currTweet => {
-      return currTweet.user == tweet.user;
-    })
-    return this.tweets[index];
+    tweet.likes.push(this.currentUser);
+    this.feedService.likeTweet(tweet).subscribe(data => {
+      console.log(data.json());
+    });
   }
 
 }
